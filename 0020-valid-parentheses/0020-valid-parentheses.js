bd84469cb191735fb3b96089a345f89f6ec51a8b
Suman Barick <codotronix@gmail.com>
2023-09-18 21:53:36 +0530

    Time: 49 ms (88.43%), Space: 42.9 MB (24.51%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    // Single bracket can't pair
+    if(s.length <= 1) return false
+
+    const stack = []
+    // let's make a map of { 'close': 'open' } of bracket pairs
+    const pairs = {
+        ')': '(', '}': '{', ']': '['
+    }
+
+    for(let i=0; i<s.length; ++i) {
+        const c = s[i]  // current char
+        // if it's an opening one
+        if(Object.values(pairs).includes(c)) stack.push(c)
+
+        // else a closing one
+        else {
+            // pop the last entry from the stack
+            const lastEntry = stack.pop()
+
+            // it must match the paris map of current char
+            if(lastEntry !== pairs[c]) {
+                return false
+            }
+        }
+    }
+
+    // if the stack is cleared, we are done
+    return (stack.length === 0) ? true : false
+};
