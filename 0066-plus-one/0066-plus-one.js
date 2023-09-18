fda636adeeceb3baa9f132cd4b8b2409b7555038
Suman Barick <codotronix@gmail.com>
2023-09-19 02:50:52 +0530

    Time: 50 ms (68.54%), Space: 41.6 MB (77.61%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -2,6 +2,11 @@
  * @param {number[]} digits
  * @return {number[]}
  */
+ // My Solution 1
+ /**
+    Runtime 57ms Beats 28.27% of users with JavaScript
+    Memory 41.45MB Beats 90.14% of users with JavaScript
+  */
 var plusOne = function(digits) {
     let carry = 1 // start with 1, as the task is to increment 1
     for(let i=digits.length-1; i>=0; --i) {
