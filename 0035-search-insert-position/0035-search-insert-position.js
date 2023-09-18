9e0526bedfded0e310786ea693d12ddc157b87a2
Suman Barick <codotronix@gmail.com>
2023-09-19 02:27:04 +0530

    Time: 44 ms (91.46%), Space: 41.8 MB (76.25%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -8,17 +8,42 @@
    Runtime 51ms Beats 62.87%of users with JavaScript
    Memory 42.06MB Beats 56.17%of users with JavaScript
  */
+// var searchInsert = function(nums, target) {
+//     for(let i=0; i<nums.length; ++i) {
+//         // if target found
+//         // OR
+//         // current number is bigger than target
+//         // the target should have been at that index 
+//         // and the current number should move to the right
+//         if(nums[i] === target || nums[i] > target) return i
+//     }
+
+//     // full array complete?
+//     // place it after everything
+//     return nums.length
+// };
+
+//////////////////////////////////////////////////////////////////////
+
+// Solution 2, Binary
 var searchInsert = function(nums, target) {
-    for(let i=0; i<nums.length; ++i) {
-        // if target found
-        // OR
-        // current number is bigger than target
-        // the target should have been at that index 
-        // and the current number should move to the right
-        if(nums[i] === target || nums[i] > target) return i
+    let left = 0
+    let right = nums.length-1
+    let mid = left + Math.floor((right-left)/2)
+
+    while(left < right) {
+        // calc mid
+        mid = left + Math.floor((right-left)/2)
+
+        if(nums[mid] === target) return mid
+
+        if(target < nums[mid]) right = mid-1
+        else left = mid+1
     }
 
-    // full array complete?
-    // place it after everything
-    return nums.length
-};
+    // this point is where left === right
+    // if target is still less than this point
+    // this point should move left leaving place for target
+    if(target <= nums[left]) return left
+    else return left + 1
+}
