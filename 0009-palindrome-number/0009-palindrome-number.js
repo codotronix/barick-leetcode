9fd66d72f134a26cd5f6e52629de424b90241c1f
Suman Barick <codotronix@gmail.com>
2023-09-18 11:53:16 +0530

    Time: 153 ms (36.66%), Space: 50.6 MB (75.9%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -2,4 +2,33 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = x => x.toString() === x.toString().split('').reverse().join('')
+
+ // 1 liner solution
+// var isPalindrome = x => x.toString() === x.toString().split('').reverse().join('')
+
+var isPalindrome = function (x) {
+    // -ve is not plaindrome
+    if(x<0) return false
+
+    // let's break the number down into an arr of digits
+    const arr = []
+    while(x>0) {
+        arr.push(x%10)
+        x = Math.floor(x/10)
+    }
+
+    return isArrPalindrome(arr)
+}
+
+function isArrPalindrome (arr) {
+    let i = 0;
+    let j = arr.length - 1;
+    
+    while(i<j) {
+        if(arr[i] !== arr[j]) return false;
+        ++i;
+        --j;
+    }
+
+    return true;
+}
