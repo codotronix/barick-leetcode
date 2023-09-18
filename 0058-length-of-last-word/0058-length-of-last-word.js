bff41e1a473d6f48632c92dbb6864e1cddfc9aee
Suman Barick <codotronix@gmail.com>
2023-09-19 02:39:25 +0530

    Time: 58 ms (21.17%), Space: 42.3 MB (12.47%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -4,8 +4,8 @@
  */
  /**
     My Solution 1
-    Runtime 48ms Beats 75.48%of users with JavaScript
-    Memory 42.02MB Beats 37.33%of users with JavaScript
+    Runtime 48ms Beats 75.48% of users with JavaScript
+    Memory 42.02MB Beats 37.33% of users with JavaScript
   */
 // var lengthOfLastWord = function(s) {
 //     s = s.trim()
@@ -20,6 +20,10 @@
 //////////////////////////////////////////////////////////
 
 // using library functions
+/**
+    Runtime 57ms Beats 25.36% of users with JavaScript
+    Memory 41.38MB Beats 94.33%of users with JavaScript
+ */
 var lengthOfLastWord = function(s) {
     let wordsArr = s.trim().split(/\s+/)    // split accross any number of spaces
     return wordsArr[wordsArr.length-1].length   // length of last word
