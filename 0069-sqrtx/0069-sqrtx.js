c8042fc1c5b968c6bbb29ba7f34072ab96916f0c
Suman Barick <codotronix@gmail.com>
2023-09-19 14:36:45 +0530

    Time: 85 ms (23.99%), Space: 43.6 MB (66.22%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -2,10 +2,14 @@
  * @param {number} x
  * @return {number}
  */
+ /**
+    Runtime 87ms Beats 21.70%of users with JavaScript
+    Memory 43.58MB Beats 66.22%of users with JavaScript
+  */
 var mySqrt = function(x) {
     if(x<2) return x
     let i=2
-    
+
     for(i=2; i*i<x; ++i);
 
     return (i*i) === x ? i : i-1
