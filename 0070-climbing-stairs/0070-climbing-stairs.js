b956b99e722f02e13dcf4c36fa8e159f4aff4e2c
Suman Barick <codotronix@gmail.com>
2023-09-19 15:16:47 +0530

    Time: 47 ms (76.78%), Space: 41.4 MB (86.46%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ /**
+    My Solution 1: DP
+  */
+var climbStairs = function(n, mem={}) {
+    if(n==2) return 2
+    if(n==1) return 1
+    if(n<=0) return 0
+    
+    if(!(n in mem)) mem[n] = climbStairs(n-1, mem) + climbStairs(n-2, mem)
+
+    return mem[n]
+};
