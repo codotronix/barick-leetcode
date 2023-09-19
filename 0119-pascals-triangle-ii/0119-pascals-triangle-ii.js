9274e05173cd6013b667104b2fed7dd4ef447309
Suman Barick <codotronix@gmail.com>
2023-09-19 23:37:27 +0530

    Time: 53 ms (50.13%), Space: 41.7 MB (70.75%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+ /**
+    My Simple Array Base Solution
+  */
+var getRow = function(rowIndex) {
+    let rowsMap = {
+        0: [1],
+        1: [1,1]
+    }
+
+    // let's build 3rd row onwards
+    for(let r=2; r<=rowIndex; ++r) {
+        // let's get the prev line array
+        let prevLine = rowsMap[r-1]
+
+        // now we know the total number of element on each line
+        // is equal to line_number
+        let thisLine = []
+        for(let c=0; c<=r; ++c) {
+            // the terminal elements is always 1
+            if(c===0 || c===r) thisLine.push(1)
+            else {
+                thisLine.push(prevLine[c-1] + prevLine[c])
+            }
+        }
+
+        // Add this line array to our cummulativeArrays
+        rowsMap[r] = thisLine
+    }
+
+    return rowsMap[rowIndex]
+};
