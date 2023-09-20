88e0bfe27988e0adae8a934417eeb434bac36bf1
Suman Barick <codotronix@gmail.com>
2023-09-21 01:20:47 +0530

    Time: 58 ms (66.08%), Space: 42.1 MB (34.44%) - LeetHub

--- 2859-add-two-promises/2859-add-two-promises.js
+++ 2859-add-two-promises/2859-add-two-promises.js
@@ -5,23 +5,23 @@
  */
  // My Solution 1
  // Runtime: 71 ms
-// var addTwoPromises = async function(promise1, promise2) {
-//     const a = await promise1
-//     const b = await promise2
-//     return a+b
-// };
+var addTwoPromises = async function(promise1, promise2) {
+    const a = await promise1
+    const b = await promise2
+    return a+b
+};
 
 // My Solution 2
-// Runtime: 104 ms
-var addTwoPromises = async function(promise1, promise2) {
-    return new Promise((resolve) => {
-        promise1.then(a => {
-            promise2.then(b => {
-                resolve(a+b)
-            })
-        })
-    })
-};
+// Time: 62 ms (44.3%), Space: 42.4 MB (9.56%)
+// var addTwoPromises = async function(promise1, promise2) {
+//     return new Promise((resolve) => {
+//         promise1.then(a => {
+//             promise2.then(b => {
+//                 resolve(a+b)
+//             })
+//         })
+//     })
+// };
 
 /**
  * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
