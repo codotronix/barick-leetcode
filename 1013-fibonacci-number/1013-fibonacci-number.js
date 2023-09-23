7ea236393172fb27e3d690930498f18187736fc4
Suman Barick <codotronix@gmail.com>
2023-09-23 22:43:27 +0530

    Time: 57 ms (55.17%), Space: 42.2 MB (10.19%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.js
+++ 1013-fibonacci-number/1013-fibonacci-number.js
@@ -3,28 +3,28 @@
  * @return {number}
  */
  // My Solution Using Recursion
-// var fib = function(n, mem={}) {
-//     if(n===0) return 0
-//     if(n===1) return 1
+var fib = function(n, mem={}) {
+    if(n===0) return 0
+    if(n===1) return 1
 
-//     if(!(n in mem)) {
-//         mem[n] = fib(n-1) + fib(n-2)
-//     }
+    if(!(n in mem)) {
+        mem[n] = fib(n-1, mem) + fib(n-2, mem)
+    }
 
-//     return mem[n]
-// };
+    return mem[n]
+};
 
 ///////////////////////////////////////////////////////////
 
 // My Solution using Loops
-var fib = function(n) {
-    let mem = { 0: 0, 1: 1 }
+// var fib = function(n) {
+//     let mem = { 0: 0, 1: 1 }
 
-    // let's build it progressively, 
-    // till we reach n
-    for(let i=2; i<=n; ++i) {
-        mem[i] = mem[i-1] + mem[i-2] 
-    }
+//     // let's build it progressively, 
+//     // till we reach n
+//     for(let i=2; i<=n; ++i) {
+//         mem[i] = mem[i-1] + mem[i-2] 
+//     }
 
-    return mem[n]
-}
+//     return mem[n]
+// }
