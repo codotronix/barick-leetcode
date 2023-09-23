fb4c0756b57063a63b249901956c4c150ed61dbf
Suman Barick <codotronix@gmail.com>
2023-09-23 22:36:38 +0530

    Time: 54 ms (62.48%), Space: 42.4 MB (5.61%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.js
+++ 1013-fibonacci-number/1013-fibonacci-number.js
@@ -2,13 +2,29 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n, mem={}) {
-    if(n===0) return 0
-    if(n===1) return 1
+ // My Solution Using Recursion
+// var fib = function(n, mem={}) {
+//     if(n===0) return 0
+//     if(n===1) return 1
 
-    if(!(n in mem)) {
-        mem[n] = fib(n-1) + fib(n-2)
+//     if(!(n in mem)) {
+//         mem[n] = fib(n-1) + fib(n-2)
+//     }
+
+//     return mem[n]
+// };
+
+///////////////////////////////////////////////////////////
+
+// My Solution using Loops
+var fib = function(n) {
+    let mem = { 0: 0, 1: 1 }
+
+    // let's build it progressively, 
+    // till we reach n
+    for(let i=2; i<=n; ++i) {
+        mem[i] = mem[i-1] + mem[i-2] 
     }
 
     return mem[n]
-};
+}
