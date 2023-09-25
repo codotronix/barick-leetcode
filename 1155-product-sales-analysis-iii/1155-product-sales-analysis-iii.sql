6a28f864de78035115987b589c1db8fe888be820
Suman Barick <codotronix@gmail.com>
2023-09-25 22:23:25 +0530

    Time: 1835 ms (75.1%), Space: 0B (100%) - LeetHub

--- 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -2,8 +2,10 @@
 SELECT S.product_id, T.first_year, S.quantity, S.price 
 FROM Sales S 
 JOIN
-(SELECT product_id, MIN(year) AS first_year
-FROM Sales
-GROUP BY product_id) AS T
+(
+    SELECT product_id, MIN(year) AS first_year
+    FROM Sales
+    GROUP BY product_id
+) AS T
 ON S.product_id = T.product_id
 AND S.year = T.first_year
