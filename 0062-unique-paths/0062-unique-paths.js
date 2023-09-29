e2b939c8a614bd2d460bce288775652714fbeaf6
Suman Barick <codotronix@gmail.com>
2023-09-29 23:51:30 +0530

    Time: 52 ms (56.86%), Space: 44.1 MB (10.97%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n, mem={ }) {
+    // base cases
+    // let (1,1) be the lowest right corner to reach
+    if(m<1 || n<1) return 0     // out of bounds
+    if(m===1 && n===1) return 1 // reached
+    if( (m===2 && n===1) || (m===1 && n===2) ) return 1
+
+    let key = [m,n]
+    if(!(key in mem)) {
+        mem[key] = uniquePaths(m-1, n, mem) + uniquePaths(m, n-1, mem)
+    }
+    return mem[key]
+};
