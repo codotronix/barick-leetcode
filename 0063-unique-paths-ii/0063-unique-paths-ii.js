eec9d2bd452977f846db2ab86147bd8922e6a00c
Suman Barick <codotronix@gmail.com>
2023-09-30 23:27:46 +0530

    Time: 55 ms (58.12%), Space: 44.3 MB (12.52%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.js
+++ 0063-unique-paths-ii/0063-unique-paths-ii.js
@@ -11,12 +11,12 @@ var uniquePathsWithObstacles = function(g, y=0, x=0, mem={}) {
 
     // if it is an obstacle, then no path ahead
     if(g[y][x] === 1) return 0
-    
+
     // if already reached
     if(y===ly && x===lx) return 1
 
     // for all other
-    const key = [y,x]
+    const key = y + '_' + x
     if(!(key in mem)) {
         // go down + go left
         mem[key] = uniquePathsWithObstacles(g, y+1, x, mem) + uniquePathsWithObstacles(g, y, x+1, mem)
