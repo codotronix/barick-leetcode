21ec13a6538277209e788657120e8f83b717a08b
Suman Barick <codotronix@gmail.com>
2024-08-10 23:00:13 +0530

    Time: 876 ms (5%), Space: 27.5 MB (99.57%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,19 +1,16 @@
 class Solution:
-    def maxArea(self, heights: List[int]) -> int:
-        # let's have 2 pointers as 2 walls on both sides
-        left = 0
-        right = len(heights) - 1
-        maxW = 0
-
-        while left < right:
-            # the smallest of wall * distance between
-            w = min(heights[left], heights[right]) * (right - left)
-            maxW = max(w, maxW)
-
-            # move the smaller one, in hope to find better
-            if heights[left] < heights[right]:
-                left += 1
-            else:
-                right -= 1
+    def maxArea(self, height: List[int]) -> int:
+        i = 0 
+        j = len(height)-1 
+        mx = 0
         
-        return maxW
+        while i<j:
+            area = (j-i) * min(height[i], height[j])
+            mx = max(mx, area)
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
+                
+        return mx
+        
