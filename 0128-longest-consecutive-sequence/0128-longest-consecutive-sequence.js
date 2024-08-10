af76612c9654c314fe48f909b675f599787f23f6
Suman Barick <codotronix@gmail.com>
2024-08-10 21:49:28 +0530

    Time: 1224 ms (21.99%), Space: 72.2 MB (75.46%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -6,22 +6,25 @@ var longestConsecutive = function(nums) {
     if(nums.length === 0) return 0
 
     // Let's put all the numbers in an object for O(1) lookup
-    let map = {}
-    for(const n of nums) map[n] = true
+    // let map = {}
+    // for(const n of nums) map[n] = true
+    let numSet = new Set(nums)  // trying with
 
     // now let's go thru each numbers
     let maxL = 0
     for(const n of nums) {
         // if the (n-1) is present in our map
         // then n is not s atrt of any sequence
-        if(map[n-1]) continue
+        // if(map[n-1]) continue
+        if(numSet.has(n-1)) continue
 
         // else it can be a start of a seq
         // so, let's see how many consecutive numbers are there
         // starting with n
         let k = n   // start num of seq
         let l = 1   // length of the seq
-        while(map[k+1]) {
+        // while(map[k+1]) {
+        while(numSet.has(k+1)) {
             ++k
             ++l
         }
