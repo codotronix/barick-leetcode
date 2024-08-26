406b46412da3f4d4b45fdc885a734714e5329896
Suman Barick <codotronix@gmail.com>
2024-08-26 22:32:10 +0530

    Time: 754 ms (41.71%), Space: 27.3 MB (85.14%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,19 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        i=0
-        j=1
-        mx=0
+        i=0     # point to the lower left
+        j=1     # point to the higher on right of i
+        mx=0    # max profit
 
         while j < len(prices):
+            # if this price is more than price at i,
+            # then, see if this is the new max
             if prices[j] > prices[i]:
                 mx = max(mx, prices[j] - prices[i])
+            # else we have found a new low (lower than current i)
+            # point i to this new low
             else:
                 i = j
+            # keep forwarding j
             j += 1
         
         return mx
