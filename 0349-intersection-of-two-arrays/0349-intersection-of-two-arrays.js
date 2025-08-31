b480c122ffc43b228f21cc47dde245dda05842a3
Suman Barick <codotronix@gmail.com>
2025-08-31 15:03:42 +0530

    Time: 2 ms (39.32%), Space: 55.8 MB (18.06%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    let m = {}
+    let r = []
+    // first list
+    for(let n of nums1) {
+        if(!(n in m)) m[n] = 1 // no duplicates, so hardcode to 1
+    }
+    // end list
+    for(let n of nums2) {
+        if(n in m && m[n] > 0) {
+            r.push(n)
+            --m[n]
+        }
+    }
+
+    return r
+};
