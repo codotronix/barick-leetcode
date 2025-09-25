fe200a31a3e0fcd4b71e8a8db854de2c6da38c4c
Suman Barick <codotronix@gmail.com>
2025-09-26 01:39:27 +0530

    Time: 14 ms (11.38%), Space: 20.9 MB (12.73%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,4 +1,7 @@
 class Solution:
+    # LESSON: do not initialize a dict on any object 
+    # in python as default params, as they gets reused 
+    # with all the function calls, even on repeated test cases
     def minimumTotal(self, triangle: List[List[int]], y=0, x=0, m=None) -> int:
         if m is None:
             m={}
@@ -6,21 +9,16 @@ class Solution:
         # if index out of bound
         # Bad Path
         if y > (len(triangle) - 1) or x > (len(triangle[y]) - 1):
-            print('inside bad path')
             return float('inf')
 
         # last row reached
         if y == (len(triangle) - 1):
-            print('returning ', triangle[y][x])
             return triangle[y][x]
         
         # use memoization
         if (y,x) not in m:
-            print('standing at ', triangle[y][x])
             m[(y,x)] = triangle[y][x] + min(self.minimumTotal(triangle, y+1, x, m), self.minimumTotal(triangle, y+1, x+1, m))
 
         return m[(y,x)]
-
-        # return triangle[y][x] + min(self.minimumTotal(triangle, y+1, x, m), self.minimumTotal(triangle, y+1, x+1, m))
 
         
