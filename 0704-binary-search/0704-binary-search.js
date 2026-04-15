b5c09af827237775959da2247a340af39faec433
Suman Barick <codotronix@gmail.com>
2026-04-15 20:38:28 +0530

    Time: 0 ms (100%), Space: 58 MB (59.7%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let l = 0;
+    let r = nums.length-1;
+
+    while(l <= r) {
+        let mid = l + Math.floor((r-l)/2);
+        if(nums[mid] === target) return mid;
+        else if(target < nums[mid]) r = mid - 1;
+        else l = mid + 1;
+    }
+
+    return -1;
+};
