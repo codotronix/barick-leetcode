fc6b9ad3dd5756133118abb178f4f735ff68a654
Suman Barick <codotronix@gmail.com>
2026-04-17 12:41:36 +0530

    Time: 0 ms (100%), Space: 54.7 MB (26.48%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -5,35 +5,33 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-/**
-    My Simple Solution which will take an addional n+m place, time complexity is 2 * O(n+m)
-    I will create a new merged sorted array, and then copy back everything to nums1
 
-    Time: 47 ms (85.59%), Space: 41.6 MB (90.3%)
- */ 
 var merge = function(nums1, m, nums2, n) {
-    let res = []
     let i=0, j=0;
 
-    // while both has something
-    while(i<m && j<n) {
-        // take the smaller number
-        if(nums1[i] < nums2[j]) {
-            res.push(nums1[i])
-            ++i
+    while (i < (m+n) && j < n) {
+        // nums[i] is already less or equal, move to next i
+        if(nums1[i] <= nums2[j]) {
+            ++i;
         }
+        // else nums[i] in greater, so shift everything from this index to 1 place right
         else {
-            res.push(nums2[j])
-            ++j
+            shiftAllRight(i, nums1);
+            nums1[i] = nums2[j];
+            ++i;
+            ++j;
         }
     }
 
-    // if something left in nums1
-    if(i<m) res = res.concat(nums1.slice(i))
-
-    // if something is left in nums2
-    if(j<n) res = res.concat(nums2.slice(j))
-
-    // copy everything back in nums1
-    for(i=0; i<m+n; ++i) nums1[i] = res[i]
+    // if i is finished, but j still remains
+    while(j < n) {
+        nums1[m+j] = nums2[j];
+        ++j;
+    }
 };
+
+function shiftAllRight(startIndex, arr) {
+    for(let i = arr.length-1; i > startIndex; i--) {
+        arr[i] = arr[i-1];
+    }
+}
