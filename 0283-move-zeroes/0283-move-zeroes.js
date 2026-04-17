f2952d68c07a3e77c3feddafebfa1d510a2a458d
Suman Barick <codotronix@gmail.com>
2026-04-17 13:01:31 +0530

    Time: 2 ms (53.31%), Space: 58.5 MB (94.63%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,24 +3,21 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    // O(n), no extra space
-    // keep the count c of the non-zero elements on the left
-    // i will scan from left to right only once
-    // so, on the left the c'th element must be a zero
-    // and i have already got past it, (that's how it was counted)
-    // Now whenever i founds a non-zero element, it should be swapped 
-    // with the c'th zero, and the count of non-zero c should be +1
-    let count = 0
-    for(let i=0; i<nums.length; ++i) {
-        if(nums[i] !== 0) {
-            // swap the count'th elements with this one
-            // the count'th element will be either this element itself
-            // until discovering the first zero
-            // and from then onwards the count'th el will always be a ZERO
-            [ nums[count], nums[i] ] = [ nums[i], nums[count] ]
-            ++count // another non-zero found, yay!
+    let i=0, j=0;
+    while(j < nums.length) {
+        // first zero is found
+        if(nums[j] === 0 && nums[i] !== 0) {
+            i = j; // point i to it
         }
-    }
+        // j finds a non-zero, and i is pointing to a zero
+        else if(nums[j] !== 0 && nums[i] === 0) {
+            nums[i] = nums[j];
+            nums[j] = 0;
+            ++i;
+        }
 
-    return nums
+        ++j;
+    }
 };
+
+// 1,2,0,0,0,3
