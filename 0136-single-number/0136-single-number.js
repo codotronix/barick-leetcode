5f932de16d43017bf3c18dbad39aabadad023218
Suman Barick <codotronix@gmail.com>
2026-04-17 15:05:06 +0530

    Time: 2 ms (58.33%), Space: 56.2 MB (62.66%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,15 +3,11 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    nums = nums.sort((a,b) => a-b)
-    let i=0
-    while(i<nums.length-1) {
-        if(nums[i] !== nums[i+1]) return nums[i]
-        i += 2
+    // let's XOR all of them,
+    // Only the ODD one out will remain
+    let r = 0;
+    for (const n of nums) {
+        r ^= n;
     }
-    // if it has reached this point
-    // then the numbers at odd places are no single
-    // but e.g. 11223, if the length was odd
-    // then we haven't checked the last number
-    return nums[nums.length-1]
+    return r;
 };
