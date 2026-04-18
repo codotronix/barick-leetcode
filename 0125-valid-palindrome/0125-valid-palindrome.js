e37c4472326494e3192a6b8ce11167e598b7fdcb
Suman Barick <codotronix@gmail.com>
2026-04-19 02:36:37 +0530

    Time: 6 ms (50.69%), Space: 57.3 MB (72.1%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,27 +3,19 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    s = s.toLowerCase() //.replace(/\s+/g, '')
-    // let's remove all the non alphabets
-    let t = ''
-    for(let c of s) {
-        if(isAlphaNum(c)) t += c
+    const chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    let cs = '';
+    for(let c of s.toLowerCase().trim()) {
+        if(chars.includes(c)) cs += c;
     }
 
-    let i=0, j=t.length-1
-    while(i<j) {
-        if(t[i] !== t[j]) return false
-        ++i;
-        --j;
+    // now do actual palidrome check on cs
+    let i = 0, j = cs.length-1;
+    while(i < j) {
+        if(cs[i] !== cs[j]) return false;
+        i++;
+        j--;
     }
 
-    return true
+    return true;
 };
-
-function isAlphaNum (ch) {
-    // a= 97, z=122
-    if(ch===' ') return false
-    if(!isNaN(ch)) return true
-    if(ch.charCodeAt(0) >= 97 && ch.charCodeAt(0) <= 122) return true
-    return false
-}
