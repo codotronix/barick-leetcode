067ffa9cb98d9c9aeef24919bf350e9ce9a5f5af
Suman Barick <codotronix@gmail.com>
2026-04-18 21:22:07 +0530

    Time: 1 ms (33.82%), Space: 57.7 MB (54.84%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -10,47 +10,42 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function(list1, list2) {
-    if(!list1 && !list2) return null
-    if(!list1) return list2
-    if(!list2) return list1
-    
-    // init
-    let h = null    // head of result
-    let p = null    // current pointer of result
-    let l1 = list1  // list1 current pointer
-    let l2 = list2 // list2 current pointer
-    
-    // while both the lists have values
-    while(l1 !== null && l2 !== null) {
-        // pick the smaller node
-        // and create a new node
-        let newNode = null
-        if(l1.val <= l2.val) {
-            newNode = new ListNode(l1.val, null)
-            l1 = l1.next
+var mergeTwoLists = function(l1, l2) {
+    if(!l1) return l2;
+    if(!l2) return l1;
+
+    let i = l1, j = l2, prev = null;
+
+    // let's merge everything back in list1
+    while(i && j) {
+        // if j.val <= i.val
+        // put this j node before this i node
+        if(j.val <= i.val) {
+            let temp = j;
+            j = j.next;
+
+            // if i is the head
+            if(i === l1) {
+                temp.next = i
+                l1 = temp;
+                prev = temp;
+            }
+            else {
+                temp.next = i;
+                prev.next = temp;
+                prev = temp;
+            }
         }
+        // else i is smaller
         else {
-            newNode = new ListNode(l2.val, null)
-            l2 = l2.next
+            prev = i;
+            i = i.next;
         }
-        
-        // create head, if head is empty
-        if(h === null) h = newNode
-        
-        // if p is not empty, link this
-        if(p !== null) p.next = newNode
-        
-        // mark this as new p
-        p = newNode
     }
-    
-    // any left overs in l1?
-    if(l1 !== null) p.next = l1
-    
-    // any left overs in l2
-    if(l2 !== null) p.next = l2
-    
-    return h
-    
+
+    // if i has finished and still some j left
+    // attach that partial list to the prev
+    if(j) prev.next = j;
+
+    return l1;
 };
