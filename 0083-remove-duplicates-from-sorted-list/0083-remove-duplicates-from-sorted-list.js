fa1533b40a7ef80c71cbd18fcfd67f44f8e5ef95
Suman Barick <codotronix@gmail.com>
2026-04-18 13:01:23 +0530

    Time: 0 ms (100%), Space: 57.3 MB (81.72%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
@@ -9,33 +9,18 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-
-/*
-    My Simple O(n) solution
-    Edited the list in place
-    Time: 58 ms (74.39%), Space: 44.4 MB (23.7%)
-*/
 var deleteDuplicates = function(head) {
-    if(!head) return head
-    
-    let p = head    // p is the last uniq node
-    let q = p       // q is the traversing node
+    let node = head;
 
-    while(q !== null) {
-        // new value found?
-        if(q.val !== p.val) {
-            // p.next should point to this new uniq node
-            p.next = q
-            // make q as new p
-            p = q
+    while(node) {
+        // if the next node is a duplicate,
+        // remove it standing here
+        if(node.next && node.next.val === node.val) {
+            node.next = node.next.next;
         }
-
-        // in any case, move q
-        q = q.next
+        // else move forward
+        else node = node.next;
     }
 
-    // we are done
-    p.next = null
-
-    return head
+    return head;
 };
