a1f954080fa31edfc717477b7567877f0aea7a04
Suman Barick <codotronix@gmail.com>
2026-04-18 22:08:26 +0530

    Time: 0 ms (100%), Space: 53.1 MB (83.93%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,23 +3,26 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    let lastL = 0;
-    let currentL = 0;
+    // let lastL = 0;
+    // let currentL = 0;
 
-    for(let c of s) {
-        // space should save and reset current word length count
-        if(c === ' ') {
-            // if currentL > 0, consider it
-            if(currentL) lastL = currentL;
-            currentL = 0;
-        }
-        else {
-            currentL++;
-        }
-    }
+    // for(let c of s) {
+    //     // space should save and reset current word length count
+    //     if(c === ' ') {
+    //         // if currentL > 0, consider it
+    //         if(currentL) lastL = currentL;
+    //         currentL = 0;
+    //     }
+    //     else {
+    //         currentL++;
+    //     }
+    // }
 
-    // if currentL > 0, consider it
-    if(currentL) lastL = currentL;
+    // // if currentL > 0, consider it
+    // if(currentL) lastL = currentL;
 
-    return lastL;
+    // return lastL;
+
+    // Also 1 liner
+    return s.trim().split(' ').at(-1).length;
 };
