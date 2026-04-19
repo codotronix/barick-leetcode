4701e16ea36e88d18a6b80b436eaacc478d0b1d0
Suman Barick <codotronix@gmail.com>
2026-04-19 16:32:17 +0530

    Time: 14 ms (55.81%), Space: 61.1 MB (11.4%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,5 +4,26 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    return s.split('').sort().join('') === t.split('').sort().join('')
+    // // 1 liner
+    // return s.split('').sort().join('') === t.split('').sort().join('')
+
+    if(s.length !== t.length) return false;
+
+    // create a map of letter count fot s
+    let m = {}
+    for(let c of s) {
+        if(!(c in m)) m[c] = 1;
+        else m[c]++;
+    }
+
+    // now keep deducting from m for t
+    for(let c of t) {
+        if(!(c in m)) return false;
+        else {
+            m[c]--;
+            if(m[c] < 0) return false;
+        }
+    }
+
+    return true;
 };
