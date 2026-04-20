df7d3c4639a90d693372ad62d56aa0bd2f8c4c7e
Suman Barick <codotronix@gmail.com>
2026-04-20 13:54:14 +0530

    Time: 420 ms (5.06%), Space: 55.4 MB (69.62%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.js
+++ 0686-repeated-string-match/0686-repeated-string-match.js
@@ -3,18 +3,84 @@
  * @param {string} b
  * @return {number}
  */
-var repeatedStringMatch = function(a, b) {
-    let count = 0;
-    let r = '';  // keep appending a
 
-    // a = 6 char
-    // b = 4 char in such a way, we need b + b + b
-    // last char of 1st b, +b, +1st char of last b
-    while(r.length <= (a.length +  2* b.length)) {
-        r += a;
-        count++;
-        if(r.includes(b)) return count;
+ var repeatedStringMatch = function(a, b) {
+    let i=0; // this will go thru b // the main string
+    let j=0; // this will go thru a // the sub string
+    let count=1; // the count of a in b
+
+    // let's find all the indices in 'a' which
+    // matches b[0]
+    // because those are the potential match start points
+    let matchedIndices = [];
+    for(let k=0; k<a.length; k++) {
+        if(a[k] === b[0]) matchedIndices.push(k);
     }
 
+    // console.log('matchedIndices = ', matchedIndices);
+
+    // no start point found ?
+    if(matchedIndices.length === 0) return -1;
+
+    // go thru b
+    let mi = 0; // track matchedIndices
+    j = matchedIndices[mi];
+    while(i < b.length) {
+        // match?
+        if(b[i] === a[j]) {
+            // console.log('match found, i=', i, ', j =', j)
+            // if this is the last char of b, BINGO
+            if(i === (b.length-1)) return count;
+
+            i++;
+            j++;
+            // j overflowed?
+            // start a new "a"
+            if(j === a.length) {
+                j=0;
+                count++;
+                // console.log('increased count to = ', count);
+            }
+        }
+        // mismatch? :(
+        else {
+            // console.log('mismatch found, i=', i, ', j =', j)
+            // mismacthing on the last char 'a'?
+            // means it's a deal breaker
+            // if(j === (a.length-1)) return -1;
+
+            // start matching i from beginning of 'a'
+            i = 0;
+            // still some mi left?
+            // if yes, then jump to the next probable start point
+            mi++;
+            if(mi >= matchedIndices.length) return -1;
+            j = matchedIndices[mi];
+            count = 1; // since we are still starting over from the 1st substring
+        }
+    }
+
+    // should never reach here
+    // console.log('reached end...')
     return -1;
-};
+ }
+
+// /**
+//  * My Method 1: Brute Force. 
+//  * Keep appending string a and check if it "includes" the string b
+//  */
+// var repeatedStringMatch_1 = function(a, b) {
+//     let count = 0;
+//     let r = '';  // keep appending a
+
+//     // a = 6 char
+//     // b = 4 char in such a way, we need b + b + b
+//     // last char of 1st b, +b, +1st char of last b
+//     while(r.length <= (a.length +  2* b.length)) {
+//         r += a;
+//         count++;
+//         if(r.includes(b)) return count;
+//     }
+
+//     return -1;
+// };
