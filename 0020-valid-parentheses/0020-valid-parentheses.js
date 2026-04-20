6bdef675d58561a44befe3080b40e47525a61e44
Suman Barick <codotronix@gmail.com>
2026-04-21 00:53:15 +0530

    Time: 3 ms (70%), Space: 58.2 MB (8.18%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -2,39 +2,23 @@
  * @param {string} s
  * @return {boolean}
  */
- /**
- Runtime 49ms Beats 88.43%of users with JavaScript,
- Memory 42.92MB Beats 24.51%of users with JavaScript
-  */
 var isValid = function(s) {
-    // Single bracket can't pair
-    if(s.length <= 1) return false
-
-    const stack = []
-    // let's make a map of { 'close': 'open' } of bracket pairs
-
-    const pairs = {
+    let matchingPair = {
         ')': '(', '}': '{', ']': '['
     }
+    let st = []
 
-    for(let i=0; i<s.length; ++i) {
-        const c = s[i]  // current char
-        // if it's an opening one
-        // if(Object.values(pairs).includes(c)) stack.push(c)
-        if(c === '(' || c === '{' || c === '[') stack.push(c)
-
-        // else a closing one
+    for(let c of s) {
+        // if a open found
+        if(c === '(' || c === '{' || c === '[') st.push(c)
+        // else a close found
         else {
-            // pop the last entry from the stack
-            const lastEntry = stack.pop()
-
-            // it must match the paris map of current char
-            if(lastEntry !== pairs[c]) {
-                return false
-            }
+            // if not a matching pair close
+            if(matchingPair[c] !== st.at(-1)) return false;
+            else st.pop()   // remove the matching open
         }
     }
 
-    // if the stack is cleared, we are done
-    return (stack.length === 0) ? true : false
+    // stack must be empty at the end
+    return st.length === 0
 };
