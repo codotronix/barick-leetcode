b322b0f0008573a3ce34dc16f8173c431dc4657f
Suman Barick <codotronix@gmail.com>
2026-04-21 13:32:10 +0530

    Time: 8 ms (36.32%), Space: 56.6 MB (80.38%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -3,45 +3,25 @@
  * @return {number}
  */
 var evalRPN = function(tokens) {
-    // if only 1 num
-    if(tokens.length === 1) return parseInt(tokens[0])
+    let st = [];
+    let op = ['+', '-', '*', '/'];
+    for(let t of tokens) {
+        // if t is an operand
+        if(op.includes(t)) {
+            const b = st.pop();
+            const a = st.pop();
+            // console.log(`${a} ${t} ${b}`);
+            let res = 0;
+            if(t === '+') res = a+b;
+            else if(t === '-') res = a-b;
+            else if(t === '*') res = a*b;
+            else res = Math.trunc(a / b); // Math.floor WILL NOT WORK here because Math.floor(-0.3) is -1 and not 0
 
-    const stack = []
-    let r
-    for(const t of tokens) {
-        // if a number, push it in stack
-        if(!isNaN(t)) stack.push(parseInt(t))
-
-        // else an operator
-        else {
-            const b = stack.pop()
-            const a = stack.pop()
-        
-            switch(t) {
-                case '+': {
-                    r = a+b
-                    break
-                }
-                case '-': {
-                    r = a-b
-                    break
-                }
-                case '*': {
-                    r = a*b
-                    break
-                }
-                case '/': {
-                    r = a/b
-                    // The division between two integers always truncates toward zero.
-                    r = r < 0 ? Math.ceil(r) : Math.floor(r)
-                    break
-                }
-            }
-            // push the result back in stack
-            stack.push(r)
-            console.log(stack)
+            st.push(res)
         }
+        // else a number
+        else st.push(Number(t));
     }
 
-    return r
+    return st.pop();
 };
