1549a3ed292862f16158a3c686b2d48665f74b4d
Suman Barick <codotronix@gmail.com>
2026-04-21 13:34:41 +0530

    Time: 6 ms (58.73%), Space: 58 MB (51.89%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -4,20 +4,18 @@
  */
 var evalRPN = function(tokens) {
     let st = [];
-    let op = ['+', '-', '*', '/'];
+    const ops = {
+        '+': (a, b) => a+b,
+        '-': (a,b) => a-b,
+        '*': (a,b) => a*b,
+        '/': (a,b) => Math.trunc(a/b)
+    };
     for(let t of tokens) {
         // if t is an operand
-        if(op.includes(t)) {
+        if(ops[t]) {
             const b = st.pop();
             const a = st.pop();
-            // console.log(`${a} ${t} ${b}`);
-            let res = 0;
-            if(t === '+') res = a+b;
-            else if(t === '-') res = a-b;
-            else if(t === '*') res = a*b;
-            else res = Math.trunc(a / b); // Math.floor WILL NOT WORK here because Math.floor(-0.3) is -1 and not 0
-
-            st.push(res)
+            st.push(ops[t](a,b))
         }
         // else a number
         else st.push(Number(t));
