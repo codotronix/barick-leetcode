023fa644c02d961d176d20543db7f1666c6f7d7e
Suman Barick <codotronix@gmail.com>
2026-04-21 21:57:23 +0530

    Time: 11 ms (6.71%), Space: 58.5 MB (15.57%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+    // 1st let's create a nextGreaterElement list
+    // for nums2
+    let stack = [];
+    let nextGreaterElement = new Array(nums2.length).fill(-1); // -1 means no greater el on right
+    for(let i=0; i<nums2.length; ++i) {
+        // if stack is empty, put this element
+        // but instead of putting the el, let's put the index
+        // by putting the index, we'll know its potition and value
+        if(!stack.length) stack.push(i);
+
+        // else if it's lesser-equal than the stack top, just push it there
+        else if(nums2[i] <= nums2[stack.at(-1)]) stack.push(i);
+        
+        // else, this is Bigger, and we have found the answer (next bigger el)
+        // for at least the top element, and let's find out if for more
+        else {
+            while(nums2[i] > nums2[stack.at(-1)]) {
+                let j = stack.pop();
+                nextGreaterElement[j] = nums2[i];
+            }
+            // don't forget to push the current el index on the stack
+            stack.push(i);
+        }
+    }
+
+    // At this point nextGreaterElement contains next bigger el 
+    // for all the el of nums2, if available, else -1
+
+    let res = new Array(nums1.length).fill(-1); // will hold the answer
+    for(let i=0; i<nums1.length; i++) {
+        for(let j=0; j<nums2.length; j++) {
+            if(nums1[i] === nums2[j]) {
+                res[i] = nextGreaterElement[j];
+            }
+        }
+    }
+
+    return res;
+};
