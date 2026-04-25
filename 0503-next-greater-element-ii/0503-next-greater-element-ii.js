e49326ca176cb1a7ce0b31c781053084f6656248
Suman Barick <codotronix@gmail.com>
2026-04-25 11:29:46 +0530

    Time: 2 ms (98.83%), Space: 61.6 MB (73.74%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.js
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.js
@@ -2,29 +2,56 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
+////// A BIT BETTER APPROACH
+//////////////////////////////
 var nextGreaterElements = function(nums) {
-    let st = [];    // monotonic stack
-    let ans = new Array(nums.length).fill(-1);
-    let i=-1   // circular array
-    let crossedOnce = false;
+    const n = nums.length;
+    const ans = new Array(n).fill(-1);
+    const stack = []; // Stores indices
 
-    while(i < nums.length) {
-        i = (++i) % nums.length;    // circular
-
-        // circlarly looping back to oneself?
-        if(st[0] === i) {
-            crossedOnce = true;
+    // Run the loop twice to simulate circularity
+    for (let i = 0; i < 2 * n; i++) {
+        const currentIndex = i % n; // Use modulo to wrap around indices
+        
+        while (stack.length > 0 && nums[currentIndex] > nums[stack[stack.length - 1]]) {
+            const prevIndex = stack.pop();
+            ans[prevIndex] = nums[currentIndex];
         }
-
-        while(st.length && nums[i] > nums[st.at(-1)]) {
-            let j = st.pop();
-            ans[j] = nums[i];
+        
+        // Only push indices during the first pass
+        // (During the second pass, we only pop to find answers for the first pass)
+        if (i < n) {
+            stack.push(currentIndex);
         }
-
-        if(crossedOnce) return ans
-
-        st.push(i); // empty or nums[i] < st.top
     }
-
+    
     return ans;
 };
+
+// var nextGreaterElements = function(nums) {
+//     let st = [];    // monotonic stack
+//     let ans = new Array(nums.length).fill(-1);
+//     let i=-1   // circular array
+//     let crossedOnce = false;
+
+//     while(i < nums.length) {
+//         i = (++i) % nums.length;    // circular
+
+//         // circlarly looping back to oneself?
+//         if(st[0] === i) {
+//             crossedOnce = true;
+//         }
+
+//         while(st.length && nums[i] > nums[st.at(-1)]) {
+//             let j = st.pop();
+//             ans[j] = nums[i];
+//         }
+
+//         if(crossedOnce) return ans
+
+//         st.push(i); // empty or nums[i] < st.top
+//     }
+
+//     return ans;
+// };
