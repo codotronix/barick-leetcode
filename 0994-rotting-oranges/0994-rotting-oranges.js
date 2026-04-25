97212b7e4c738a84dd19b992f052fe6023df4a93
Suman Barick <codotronix@gmail.com>
2026-04-25 12:41:15 +0530

    Time: 4 ms (86.69%), Space: 58.7 MB (48.4%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.js
+++ 0994-rotting-oranges/0994-rotting-oranges.js
@@ -4,14 +4,14 @@
  */
 var orangesRotting = function(grid) {
     let bads = [];
-    let goods = {};
-    // let visited = {};
+    let goodCount = 0;
     let mins = 0;
+
     // find the bads and goods
     for(let y=0; y<grid.length; y++) {
         for(let x=0; x<grid[0].length; x++) {
             if(grid[y][x] === 1) {
-                goods[[y,x]] = true;
+                goodCount++;
             }
             else if(grid[y][x] === 2) {
                 bads.push([y,x]);
@@ -20,40 +20,45 @@ var orangesRotting = function(grid) {
     }
 
     // no bads, only goods?
-    if(!bads.length && Object.keys(goods).length) return -1;
+    if(!bads.length && goodCount) return -1;
 
-    // loop thru the bads
-    while(bads.length) {
+    // while there goods and bads
+    while(bads.length && goodCount) {
         let corruptOne = false;
         let newBads = []; 
 
+        // loop thru the bads,
         for(let bad of bads) {
             let [y, x] = bad;
 
             // try up
-            if((y-1) >= 0 && goods[[y-1, x]]) {
-                delete goods[[y-1, x]];
+            if((y-1) >= 0 && grid[y-1][x] === 1) {
+                goodCount--;
+                grid[y-1][x] = 2;
                 newBads.push([y-1, x]);
                 corruptOne = true;
             }
             
             // try down
-            if((y+1) < grid.length && goods[[y+1, x]]) {
-                delete goods[[y+1, x]];
+            if((y+1) < grid.length && grid[y+1][x] === 1) {
+                goodCount--;
+                grid[y+1][x] = 2;
                 newBads.push([y+1, x]);
                 corruptOne = true;
             }
 
             // try left
-            if((x-1) >= 0 && goods[[y, x-1]]) {
-                delete goods[[y, x-1]];
+            if((x-1) >= 0 && grid[y][x-1] === 1) {
+                goodCount--;
+                grid[y][x-1] = 2;
                 newBads.push([y, x-1]);
                 corruptOne = true;
             }
             
             // try right
-            if((x+1) < grid[0].length && goods[[y, x+1]]) {
-                delete goods[[y, x+1]];
+            if((x+1) < grid[0].length && grid[y][x+1] === 1) {
+                goodCount--;
+                grid[y][x+1] = 2;
                 newBads.push([y, x+1]);
                 corruptOne = true;
             }
@@ -63,12 +68,11 @@ var orangesRotting = function(grid) {
         if(corruptOne) mins++;
 
         // no goods left?
-        let remainingGood = Object.keys(goods).length;
-        if(!remainingGood) return mins;
+        if(!goodCount) return mins;
 
         // if couldn't corrupt any new apple in this min
         // and still good left, then they are unreachable
-        if(remainingGood && !corruptOne) return -1;
+        if(goodCount && !corruptOne) return -1;
 
         bads = newBads;
     }
